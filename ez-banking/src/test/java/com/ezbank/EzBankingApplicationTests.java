9eda80f8dece9862c232d3a64b1f393687b15908
Manas Mohanty <manasmohanty07@gmail.com>
2023-08-08 17:37:23 +0530

    added angular code

--- ez-banking/src/test/java/com/ezbank/EzBankingApplicationTests.java
+++ ez-banking/src/test/java/com/ezbank/EzBankingApplicationTests.java
@@ -3,10 +3,10 @@ package com.ezbank;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EzBankingApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
